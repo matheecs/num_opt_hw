9085ad95fbf64b017f0d917b468d8cf7ef85709f
matheecs <matheecs@qq.com>
2022-07-29 16:54:55 +0800

    fix some bugs

--- include/gcopter/cubic_spline.hpp
+++ include/gcopter/cubic_spline.hpp
@@ -144,203 +144,220 @@ namespace cubic_spline {
 
 // clang-format on
 
-    class CubicSpline {
-    public:
-        CubicSpline() = default;
+class CubicSpline {
+ public:
+  CubicSpline() = default;
 
-        ~CubicSpline() { A.destroy(); }
+  ~CubicSpline() { A.destroy(); }
 
-    private:
-        int N;
-        Eigen::Vector2d headP;
-        Eigen::Vector2d tailP;
-        BandedSystem A;
-        Eigen::MatrixX2d b;
-        Eigen::Matrix2Xd inner_points;
-        std::vector <Eigen::Matrix<double, 2, 4>> cMats;
-        Eigen::MatrixXd A_inv, A_inv_3;
-        Eigen::MatrixXd M_c_x_first, M_d_x_first, M_D_x;
+ private:
+  int N;
+  Eigen::Vector2d headP;
+  Eigen::Vector2d tailP;
+  BandedSystem A;
+  Eigen::MatrixX2d b;
+  Eigen::Matrix2Xd inner_points;
+  std::vector<Eigen::Matrix<double, 2, 4>> cMats;
+  Eigen::MatrixXd A_inv;
+  Eigen::MatrixXd M_c_x_first, M_d_x_first, M_D_x;
 
-    public:
-        inline void setConditions(const Eigen::Vector2d &headPos,
-                                  const Eigen::Vector2d &tailPos,
-                                  const int &pieceNum) {
-            // TODO
-            headP = headPos;
-            tailP = tailPos;
-            N = pieceNum;
-            A.create(N - 1, 3, 3);
-            b.resize(N - 1, 2);
-            cMats.resize(N);
+ public:
+  inline void setConditions(const Eigen::Vector2d &headPos,
+                            const Eigen::Vector2d &tailPos,
+                            const int &pieceNum) {
+    // TODO
+    headP = headPos;
+    tailP = tailPos;
+    N = pieceNum;
 
-            Eigen::MatrixXd A_m;
-            A_m.resize(N - 1, N - 1);
-            A_m.setZero();
-            A_m(0, 0) = 4;
-            A_m(0, 1) = 1;
-            for (int i = 1; i < N - 2; ++i) {
-                A_m(i, i - 1) = 1;
-                A_m(i, i) = 4;
-                A_m(i, i + 1) = 1;
-            }
-            A_m(N - 2, N - 3) = 1;
-            A_m(N - 2, N - 2) = 4;
-            A_inv = A_m;
-            A_inv_3 = 3 * A_inv;
+    std::cout << "N = " << N << std::endl;
 
-            M_c_x_first.resize(N, N - 1);
-            M_c_x_first.setZero();
-            M_c_x_first(0, 0) = 3;
-            M_c_x_first(N - 1, N - 2) = -3;
-            for (int i = 1; i <= N - 2; ++i) {
-                M_c_x_first(i, i - 1) = -3;
-                M_c_x_first(i, i) = 3;
-            }
+    A.create(N - 1, 3, 3);
+    b.resize(N - 1, 2);
+    cMats.resize(N);
 
-            M_d_x_first.resize(N, N - 1);
-            M_d_x_first.setZero();
-            M_d_x_first(0, 0) = -2;
-            M_d_x_first(N - 1, N - 2) = 2;
-            for (int i = 1; i <= N - 2; ++i) {
-                M_d_x_first(i, i - 1) = 2;
-                M_d_x_first(i, i) = -2;
-            }
+    Eigen::MatrixXd A_m;
+    A_m.resize(N - 1, N - 1);
+    A_m.setZero();
+    A_m(0, 0) = 4;
+    A_m(0, 1) = 1;
+    for (int i = 1; i < N - 2; ++i) {
+      A_m(i, i - 1) = 1;
+      A_m(i, i) = 4;
+      A_m(i, i + 1) = 1;
+    }
+    A_m(N - 2, N - 3) = 1;
+    A_m(N - 2, N - 2) = 4;
+    A_inv = A_m.inverse();
 
-            M_D_x.resize(N - 1, N - 1);
-            M_D_x.setZero();
-            M_D_x(0, 1) = 1;
-            M_D_x(N - 2, N - 3) = 1;
-            for (int i = 1; i <= N - 3; ++i) {
-                M_D_x(i, i - 1) = -1;
-                M_D_x(i, i + 1) = 1;
-            }
-            M_D_x = 3 * A_inv * M_D_x;
+    std::cout << "A = \n" << A_m << std::endl;
+    std::cout << "A_inv = \n" << A_inv << std::endl;
 
-            return;
-        }
+    M_c_x_first.resize(N, N - 1);
+    M_c_x_first.setZero();
+    M_c_x_first(0, 0) = 3;
+    M_c_x_first(N - 1, N - 2) = -3;
+    for (int i = 1; i <= N - 2; ++i) {
+      M_c_x_first(i, i - 1) = -3;
+      M_c_x_first(i, i) = 3;
+    }
 
-        inline void setInnerPoints(const Eigen::Ref<const Eigen::Matrix2Xd> &inPs) {
-            inner_points = inPs;
-            // TODO
-            A.reset();
-            b.setZero();
+    std::cout << "M_c_x_first = \n" << M_c_x_first << std::endl;
 
-            A(0, 0) = 4;
-            A(0, 1) = 1;
-            for (int i = 1; i < N - 2; ++i) {
-                A(i, i - 1) = 1;
-                A(i, i) = 4;
-                A(i, i + 1) = 1;
-            }
-            A(N - 2, N - 3) = 1;
-            A(N - 2, N - 2) = 4;
+    M_d_x_first.resize(N, N - 1);
+    M_d_x_first.setZero();
+    M_d_x_first(0, 0) = -2;
+    M_d_x_first(N - 1, N - 2) = 2;
+    for (int i = 1; i <= N - 2; ++i) {
+      M_d_x_first(i, i - 1) = 2;
+      M_d_x_first(i, i) = -2;
+    }
 
-            b.row(0) = 3 * (inPs.col(1).transpose() - headP.transpose());
-            for (int i = 1; i < N - 2; ++i) {
-                b.row(i) =
-                        3 * (inPs.col(i + 1).transpose() - inPs.col(i - 1).transpose());
-            }
-            b.row(N - 2) = 3 * (tailP.transpose() - inPs.col(N - 3).transpose());
+    std::cout << "M_d_x_first = \n" << M_d_x_first << std::endl;
 
-            A.factorizeLU();
-            A.solve(b);
+    M_D_x.resize(N - 1, N - 1);
+    M_D_x.setZero();
+    M_D_x(0, 1) = 1;
+    M_D_x(N - 2, N - 3) = -1;
+    for (int i = 1; i <= N - 3; ++i) {
+      M_D_x(i, i - 1) = -1;
+      M_D_x(i, i + 1) = 1;
+    }
+    std::cout << "M_D_x(raw) = \n" << M_D_x << std::endl;
+    M_D_x = 3 * A_inv * M_D_x;
 
-            Eigen::Matrix<double, 2, 4> coeffMat;
+    std::cout << "M_D_x = \n" << M_D_x << std::endl;
 
-            // i = 0
-            coeffMat.col(0) = headP;
-            coeffMat.col(1) = Eigen::Vector2d::Zero();
-            coeffMat.col(2) = 3 * (inner_points.col(0) - headP) - b.row(0).transpose();
-            coeffMat.col(3) = 3 * (headP - inner_points.col(0)) + b.row(0).transpose();
-            cMats[0] = coeffMat;
+    return;
+  }
 
-            // i = 1->(N-2)
-            for (int i = 1; i <= N - 2; ++i) {
-                coeffMat.col(0) = inner_points.col(i - 1);
-                coeffMat.col(1) = b.row(i - 1).transpose();
-                coeffMat.col(2) = 3 * (inner_points.col(i) - inner_points.col(i - 1)) -
-                                  2 * b.row(i - 1).transpose() - b.row(i).transpose();
-                coeffMat.col(3) = 2 * (inner_points.col(i - 1) - inner_points.col(i)) +
-                                  1 * b.row(i - 1).transpose() + b.row(i).transpose();
-                cMats[i] = coeffMat;
-            }
+  inline void setInnerPoints(const Eigen::Ref<const Eigen::Matrix2Xd> &inPs) {
+    inner_points = inPs;
+    // TODO
+    A.reset();
+    b.setZero();
 
-            // i = N-1
-            coeffMat.col(0) = inner_points.col(N - 2);
-            coeffMat.col(1) = b.row(N - 2).transpose();
-            coeffMat.col(2) =
-                    3 * (tailP - inner_points.col(N - 2)) - 2 * b.row(N - 2).transpose();
-            coeffMat.col(3) =
-                    2 * (inner_points.col(N - 2) - tailP) + 1 * b.row(N - 2).transpose();
-            cMats[N - 1] = coeffMat;
+    A(0, 0) = 4;
+    A(0, 1) = 1;
+    for (int i = 1; i < N - 2; ++i) {
+      A(i, i - 1) = 1;
+      A(i, i) = 4;
+      A(i, i + 1) = 1;
+    }
+    A(N - 2, N - 3) = 1;
+    A(N - 2, N - 2) = 4;
 
-            return;
-        }
+    b.row(0) = 3 * (inPs.col(1).transpose() - headP.transpose());
+    for (int i = 1; i < N - 2; ++i) {
+      b.row(i) =
+          3 * (inPs.col(i + 1).transpose() - inPs.col(i - 1).transpose());
+    }
+    b.row(N - 2) = 3 * (tailP.transpose() - inPs.col(N - 3).transpose());
 
-        inline void getCurve(CubicCurve &curve) const {
-            // TODO
-            std::vector<double> durs(N, 1.0);
-            auto c = CubicCurve(durs, cMats);
-            curve = std::move(c);
+    A.factorizeLU();
+    A.solve(b);
 
-            return;
-        }
+    Eigen::Matrix<double, 2, 4> coeffMat;
 
-        inline void getStretchEnergy(double &energy) const {
-            // TODO
-            double E{0};
-            for (int i = 0; i < N; ++i) {
-                // 4*c*c + 12*c*d + 12*d*d
-                E += 4 * cMats[i].col(2).dot(cMats[i].col(2))
-                     + 12 * cMats[i].col(2).dot(cMats[i].col(3))
-                     + 12 * cMats[i].col(3).dot(cMats[i].col(3));
-            }
-            return;
-        }
+    // i = 0
+    coeffMat.col(3) = headP;
+    coeffMat.col(2) = Eigen::Vector2d::Zero();
+    coeffMat.col(1) = 3 * (inner_points.col(0) - headP) - b.row(0).transpose();
+    coeffMat.col(0) = 3 * (headP - inner_points.col(0)) + b.row(0).transpose();
+    // d-c-b-a
+    cMats[0] = coeffMat;
 
-        inline const Eigen::MatrixX2d &getCoeffs(void) const { return b; }
+    // i = 1->(N-2)
+    for (int i = 1; i <= N - 2; ++i) {
+      coeffMat.col(3) = inner_points.col(i - 1);
+      coeffMat.col(2) = b.row(i - 1).transpose();
+      coeffMat.col(1) = 3 * (inner_points.col(i) - inner_points.col(i - 1)) -
+                        2 * b.row(i - 1).transpose() - b.row(i).transpose();
+      coeffMat.col(0) = 2 * (inner_points.col(i - 1) - inner_points.col(i)) +
+                        1 * b.row(i - 1).transpose() + b.row(i).transpose();
+      cMats[i] = coeffMat;
+    }
 
-        inline void getGrad(Eigen::Ref <Eigen::Matrix2Xd> gradByPoints) const {
-            // TODO
-            // Energy -> 8cc'+12c'd+12cd'+24dd'
-            gradByPoints.setZero();
-            // for piece0
-            std::cout << "add piece0" << std::endl;
-            Eigen::VectorXd c_diff, d_diff;
-            c_diff = M_c_x_first.row(0).transpose() - M_D_x.row(0).transpose();
-            d_diff = M_d_x_first.row(0).transpose() + M_D_x.row(0).transpose();
+    // i = N-1
+    coeffMat.col(3) = inner_points.col(N - 2);
+    coeffMat.col(2) = b.row(N - 2).transpose();
+    coeffMat.col(1) =
+        3 * (tailP - inner_points.col(N - 2)) - 2 * b.row(N - 2).transpose();
+    coeffMat.col(0) =
+        2 * (inner_points.col(N - 2) - tailP) + 1 * b.row(N - 2).transpose();
+    cMats[N - 1] = coeffMat;
 
-            std::cout << "8*XXX=" << cMats[0].col(2) * c_diff.transpose() << std::endl;
-            std::cout << "gradByPoints=" << gradByPoints << std::endl;
+    // std::cout << "cMat BEGIN\n" << std::endl;
+    // for (const auto &cMat : cMats) {
+    //   std::cout << cMat << std::endl;
+    //   std::cout << "===" << std::endl;
+    // }
+    // std::cout << "cMat END\n" << std::endl;
 
-            gradByPoints +=
-                    8 * cMats[0].col(2) * c_diff.transpose()
-                    + 12 * cMats[0].col(3) * c_diff.transpose()
-                    + 12 * cMats[0].col(2) * d_diff.transpose()
-                    + 24 * cMats[0].col(3) * d_diff.transpose();
-            // for piece1->(N-2)
-            std::cout << "add piece1->(N-2)" << std::endl;
-            for (int i = 1; i < N - 2; ++i) {
-                c_diff = M_c_x_first.row(i).transpose() - 2 * M_D_x.row(i - 1).transpose() - M_D_x.row(i).transpose();
-                d_diff = M_d_x_first.row(i).transpose() + 1 * M_D_x.row(i - 1).transpose() + M_D_x.row(i).transpose();
-                gradByPoints +=
-                        8 * cMats[i].col(2) * c_diff.transpose()
-                        + 12 * cMats[i].col(3) * c_diff.transpose()
-                        + 12 * cMats[i].col(2) * d_diff.transpose()
-                        + 24 * cMats[i].col(3) * d_diff.transpose();
-            }
-            // for piece(N-1)
-            std::cout << "piece N-1" << std::endl;
-            c_diff = M_c_x_first.row(N - 1).transpose() - 2 * M_D_x.row(N - 2).transpose();
-            d_diff = M_d_x_first.row(N - 1).transpose() + 1 * M_D_x.row(N - 2).transpose();
-            gradByPoints +=
-                    8 * cMats[N - 1].col(2) * c_diff.transpose()
-                    + 12 * cMats[N - 1].col(3) * c_diff.transpose()
-                    + 12 * cMats[N - 1].col(2) * d_diff.transpose()
-                    + 24 * cMats[N - 1].col(3) * d_diff.transpose();
-            return;
-        }
-    };
+    return;
+  }
+
+  inline void getCurve(CubicCurve &curve) const {
+    // TODO
+    std::vector<double> durs(N, 1.0);
+    auto c = CubicCurve(durs, cMats);
+    curve = std::move(c);
+    std::cout << "curve = \n" << curve.getPositions() << std::endl;
+
+    return;
+  }
+
+  inline void getStretchEnergy(double &energy) const {
+    // TODO
+    double E{0};
+    for (int i = 0; i < N; ++i) {
+      // 4*c*c + 12*c*d + 12*d*d
+      E += 4 * cMats[i].col(1).dot(cMats[i].col(1)) +
+           12 * cMats[i].col(1).dot(cMats[i].col(0)) +
+           12 * cMats[i].col(0).dot(cMats[i].col(0));
+    }
+    energy = E;
+    return;
+  }
+
+  inline const Eigen::MatrixX2d &getCoeffs(void) const { return b; }
+
+  inline void getGrad(Eigen::Ref<Eigen::Matrix2Xd> gradByPoints) const {
+    // TODO
+    // Energy -> 8cc'+12c'd+12cd'+24dd'
+    gradByPoints.setZero();
+    // Piece0
+    Eigen::VectorXd c_diff, d_diff;
+    c_diff = M_c_x_first.row(0).transpose() - M_D_x.row(0).transpose();
+    d_diff = M_d_x_first.row(0).transpose() + M_D_x.row(0).transpose();
+
+    gradByPoints += 8 * cMats[0].col(1) * c_diff.transpose() +
+                    12 * cMats[0].col(0) * c_diff.transpose() +
+                    12 * cMats[0].col(1) * d_diff.transpose() +
+                    24 * cMats[0].col(0) * d_diff.transpose();
+    // Piece1->(N-2)
+    for (int i = 1; i <= N - 2; ++i) {
+      c_diff = M_c_x_first.row(i).transpose() -
+               2 * M_D_x.row(i - 1).transpose() - M_D_x.row(i).transpose();
+      d_diff = M_d_x_first.row(i).transpose() +
+               1 * M_D_x.row(i - 1).transpose() + M_D_x.row(i).transpose();
+      gradByPoints += 8 * cMats[i].col(1) * c_diff.transpose() +
+                      12 * cMats[i].col(0) * c_diff.transpose() +
+                      12 * cMats[i].col(1) * d_diff.transpose() +
+                      24 * cMats[i].col(0) * d_diff.transpose();
+    }
+    // Piece(N-1)
+    c_diff =
+        M_c_x_first.row(N - 1).transpose() - 2 * M_D_x.row(N - 2).transpose();
+    d_diff =
+        M_d_x_first.row(N - 1).transpose() + 1 * M_D_x.row(N - 2).transpose();
+    gradByPoints += 8 * cMats[N - 1].col(1) * c_diff.transpose() +
+                    12 * cMats[N - 1].col(0) * c_diff.transpose() +
+                    12 * cMats[N - 1].col(1) * d_diff.transpose() +
+                    24 * cMats[N - 1].col(0) * d_diff.transpose();
+    return;
+  }
+};
 
 // clang-format off
 }
--- include/gcopter/path_smoother.hpp
+++ include/gcopter/path_smoother.hpp
@@ -1,6 +1,7 @@
 #ifndef PATH_SMOOTHER_HPP
 #define PATH_SMOOTHER_HPP
 
+#include <iomanip>
 #include "cubic_spline.hpp"
 #include "lbfgs.hpp"
 
@@ -13,161 +14,160 @@
 
 namespace path_smoother {
 
-    class PathSmoother {
-    private:
-        cubic_spline::CubicSpline cubSpline;
+class PathSmoother {
+ private:
+  cubic_spline::CubicSpline cubSpline;
 
-        int pieceN;
-        Eigen::Matrix3Xd diskObstacles;
-        double penaltyWeight;
-        Eigen::Vector2d headP;
-        Eigen::Vector2d tailP;
-        Eigen::Matrix2Xd points;
-        Eigen::Matrix2Xd gradByPoints;
+  int pieceN;
+  Eigen::Matrix3Xd diskObstacles;
+  double penaltyWeight;
+  Eigen::Vector2d headP;
+  Eigen::Vector2d tailP;
+  Eigen::Matrix2Xd points;
+  Eigen::Matrix2Xd gradByPoints;
 
-        lbfgs::lbfgs_parameter_t lbfgs_params;
+  lbfgs::lbfgs_parameter_t lbfgs_params;
 
-    private:
-        static inline double costFunction(void *ptr, const Eigen::VectorXd &x,
-                                          Eigen::VectorXd &g) {
-            // TODO
-            const int n = x.size();
-            Eigen::VectorXd x_raw = x;
-            const int num_inner_points = n / 2;
-            Eigen::Map <Eigen::MatrixXd> inner_points(x_raw.data(), 2, num_inner_points);
-            PathSmoother *pM = (PathSmoother *) ptr;
+ private:
+  static inline double costFunction(void *ptr, const Eigen::VectorXd &x,
+                                    Eigen::VectorXd &g) {
+    // TODO
+    // std::cout << std::setprecision(4) << "x_current:" << x.transpose()
+    //           << std::endl;
+    const int n = x.size();
+    const int num_inner_points = n / 2;
+    Eigen::MatrixXd inner_points;
+    inner_points.resize(2, num_inner_points);
+    inner_points.row(0) = x.head(num_inner_points);
+    inner_points.row(1) = x.tail(num_inner_points);
 
-            pM->cubSpline.setInnerPoints(inner_points);
+    // std::cout << "inner_points:\n" << inner_points << std::endl;
+    auto instance = reinterpret_cast<path_smoother::PathSmoother *>(ptr);
 
-            std::cout << "pieceN = " << pM->pieceN << std::endl;
+    instance->cubSpline.setInnerPoints(inner_points);
 
-            Eigen::Matrix2Xd g_raw;
-            g_raw.resize(2, num_inner_points);
-            g_raw.setZero();
+    Eigen::Matrix2Xd g_raw;
+    g_raw.resize(2, num_inner_points);
+    g_raw.setZero();
 
-            std::cout << "Start add cost and grad" << std::endl;
+    double cost = 0.0;
 
-            // Add Potential
-            double cost = 0.0;
-            Eigen::Matrix2Xd gradByPotential;
-            gradByPotential.resize(2, num_inner_points);
-            gradByPotential.setZero();
-            std::cout << "before for loop" << std::endl;
-            for (int i = 0; i < num_inner_points; ++i) {
-                for (int j = 0; j < pM->diskObstacles.cols(); ++j) {
-                    Eigen::Vector2d diff = inner_points.col(i) - pM->diskObstacles.col(j).head(2);
-                    double distance = diff.norm();
-                    double delta = pM->diskObstacles(2, j) - distance;
+    // Add Potential
+    Eigen::Matrix2Xd gradByPotential;
+    gradByPotential.resize(2, num_inner_points);
+    gradByPotential.setZero();
 
-                    std::cout << "running" << std::endl;
+    for (int i = 0; i < num_inner_points; ++i) {
+      for (int j = 0; j < instance->diskObstacles.cols(); ++j) {
+        Eigen::Vector2d diff =
+            inner_points.col(i) - instance->diskObstacles.col(j).head(2);
+        double distance = diff.norm();
+        double delta = instance->diskObstacles(2, j) - distance;
 
-                    if (delta > 0.0) {
-                        cost += delta;
-                        gradByPotential.col(i) += (-diff / distance);
-                    }
-                }
-            }
-            cost *= pM->penaltyWeight;
-            g_raw += pM->penaltyWeight * gradByPotential;
-
-            std::cout << "add E" << std::endl;
-
-            // Add Energy
-            double energy = 0.0;
-            pM->cubSpline.getStretchEnergy(energy);
-            cost += energy;
-
-            std::cout << "todo add grad of E" << std::endl;
-
-            Eigen::Matrix2Xd gradByEnergy;
-            gradByEnergy.resize(2, pM->pieceN - 1);
-            pM->cubSpline.getGrad(gradByEnergy);
-            g_raw += gradByEnergy;
-
-            g.head(num_inner_points) = g_raw.row(0).transpose();
-            g.tail(num_inner_points) = g_raw.row(1).transpose();
-
-            std::cout << "ok" << std::endl;
-
-            return cost;
+        if (delta > 0.0) {
+          cost += delta;
+          gradByPotential.col(i) += (-diff / distance);
         }
+      }
+    }
+    // std::cout << "potential(without weight):" << cost << std::endl;
+    // std::cout << "potentialGrad:(wo weight)\n" << gradByPotential <<
+    // std::endl;
+    cost *= instance->penaltyWeight;
 
-        static int monitorProgress(void *instance,
-                                   const Eigen::VectorXd &x,
-                                   const Eigen::VectorXd &g,
-                                   const double fx,
-                                   const double step,
-                                   const int k,
-                                   const int ls) {
-            std::cout << std::setprecision(4)
-                      << "================================" << std::endl
-                      << "Iteration: " << k << std::endl
-                      << "Function Value: " << fx << std::endl
-                      << "Gradient Inf Norm: " << g.cwiseAbs().maxCoeff() << std::endl
-                      << "Variables: " << std::endl
-                      << x.transpose() << std::endl;
-            return 0;
-        }
+    g_raw += instance->penaltyWeight * gradByPotential;
 
-    public:
-        inline bool setup(const Eigen::Vector2d &initialP,
-                          const Eigen::Vector2d &terminalP, const int &pieceNum,
-                          const Eigen::Matrix3Xd &diskObs, const double penaWeight) {
-            pieceN = pieceNum;
-            diskObstacles = diskObs;
-            penaltyWeight = penaWeight;
-            headP = initialP;
-            tailP = terminalP;
+    // Add Energy
+    double energy = 0.0;
+    instance->cubSpline.getStretchEnergy(energy);
+    // std::cout << "energy:" << energy << std::endl;
+    cost += energy;
 
-            cubSpline.setConditions(headP, tailP, pieceN);
+    Eigen::Matrix2Xd gradByEnergy;
+    gradByEnergy.resize(2, instance->pieceN - 1);
+    instance->cubSpline.getGrad(gradByEnergy);
+    g_raw += gradByEnergy;
 
-            points.resize(2, pieceN - 1);
-            gradByPoints.resize(2, pieceN - 1);
+    // std::cout << "energyGrad:\n" << gradByEnergy << std::endl;
 
-            return true;
-        }
+    // std::cout << "total cost(potential+energy):" << cost << std::endl;
+    // std::cout << "total grad\n" << g_raw << std::endl;
 
-        inline double optimize(CubicCurve &curve, const Eigen::Matrix2Xd &iniInPs,
-                               const double &relCostTol) {
-            // TODO
-            double finalCost;
-            std::cout << iniInPs << std::endl;
-            /* Set the initial guess */
-            std::cout << "Set the initial guess" << std::endl;
-            Eigen::VectorXd x(Eigen::VectorXd::Zero(2 * (pieceN - 1)));
-            x.head(pieceN - 1) = iniInPs.row(0).transpose();
-            x.tail(pieceN - 1) = iniInPs.row(1).transpose();
+    g.head(num_inner_points) = g_raw.row(0).transpose();
+    g.tail(num_inner_points) = g_raw.row(1).transpose();
 
-            /* Set the minimization parameters */
-            std::cout << "Set the minimization parameters" << std::endl;
-            lbfgs::lbfgs_parameter_t params;
-            params.g_epsilon = 1.0e-8;
-            params.past = 3;
-            params.delta = relCostTol;
+    return cost;
+  }
 
-            /* Start minimization */
-            std::cout << "Start minimization" << std::endl;
-            int ret = lbfgs::lbfgs_optimize(x,
-                                            finalCost,
-                                            &costFunction,
-                                            monitorProgress,
-                                            this,
-                                            params);
+  static int monitorProgress(void *instance, const Eigen::VectorXd &x,
+                             const Eigen::VectorXd &g, const double fx,
+                             const double step, const int k, const int ls) {
+    std::cout << std::setprecision(4)
+              << "================================" << std::endl
+              << "Iteration: " << k << std::endl
+              << "Function Value: " << fx << std::endl
+              << "Gradient Inf Norm: " << g.cwiseAbs().maxCoeff() << std::endl
+              << "Variables: " << std::endl
+              << x.transpose() << std::endl;
+    return 0;
+  }
 
-            /* Report the result. */
-            std::cout << std::setprecision(4)
-                      << "================================" << std::endl
-                      << "L-BFGS Optimization Returned: " << ret << std::endl
-                      << "Minimized Cost: " << finalCost << std::endl
-                      << "Optimal Variables: " << std::endl
-                      << x.transpose() << std::endl;
+ public:
+  inline bool setup(const Eigen::Vector2d &initialP,
+                    const Eigen::Vector2d &terminalP, const int &pieceNum,
+                    const Eigen::Matrix3Xd &diskObs, const double penaWeight) {
+    pieceN = pieceNum;
+    diskObstacles = diskObs;
+    penaltyWeight = penaWeight;
+    headP = initialP;
+    tailP = terminalP;
 
-            cubSpline.getCurve(curve);
+    cubSpline.setConditions(headP, tailP, pieceN);
 
+    points.resize(2, pieceN - 1);
+    gradByPoints.resize(2, pieceN - 1);
 
-            return finalCost;
-        }
-    };
+    std::cout << "diskObstacles:\n" << diskObstacles << std::endl;
+
+    return true;
+  }
+
+  inline double optimize(CubicCurve &curve, const Eigen::Matrix2Xd &iniInPs,
+                         const double &relCostTol) {
+    // TODO
+    double finalCost;
+    std::cout << "Initial points = \n" << iniInPs << std::endl;
+    /* Set the initial guess */
+    std::cout << "Set the initial guess" << std::endl;
+    Eigen::VectorXd x(Eigen::VectorXd::Zero(2 * (pieceN - 1)));
+    x.head(pieceN - 1) = iniInPs.row(0).transpose();
+    x.tail(pieceN - 1) = iniInPs.row(1).transpose();
+
+    /* Set the minimization parameters */
+    std::cout << "Set the minimization parameters" << std::endl;
+    lbfgs::lbfgs_parameter_t params;
+    params.g_epsilon = 1.0e-8;
+    params.past = 3;
+    params.delta = relCostTol;
+
+    /* Start minimization */
+    std::cout << "Start minimization" << std::endl;
+    int ret = lbfgs::lbfgs_optimize(x, finalCost, costFunction, monitorProgress,
+                                    this, params);
+
+    /* Report the result. */
+    std::cout << std::setprecision(4)
+              << "================================" << std::endl
+              << "L-BFGS Optimization Returned: " << ret << std::endl
+              << "Minimized Cost: " << finalCost << std::endl
+              << "Optimal Variables: " << std::endl
+              << x.transpose() << std::endl;
+
+    cubSpline.getCurve(curve);
+
+    return finalCost;
+  }
+};
 
 }  // namespace path_smoother
 
